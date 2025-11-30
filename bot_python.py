b81308ce59702a18d1d95b2d62d3d6c6a28a7f37
Artur <PC Work>
2025-11-30 17:51:53 +0300

    главное меню редактирование

--- bot_python.py
+++ bot_python.py
@@ -21,33 +21,49 @@ def start_proceed(message):
     bot.send_message(user_id, message_text, reply_markup=kb)
 
 
+@bot.callback_query_handler(func=lambda call: call.data == "back_to_main")
+def start_proceed(call):
+    message_text = '''Привет!
+Этот бот генерирует принципиальную схему функционального блока IEC-61499 на основе его XML файла'''
+
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton('Загрузить xml', callback_data='load_xml'))
+    kb.add(types.InlineKeyboardButton('Помощь', callback_data='help'))
+    kb.add(types.InlineKeyboardButton('О проекте', callback_data='project_about'))
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_text, reply_markup=kb)
+
+
 @bot.callback_query_handler(func=lambda call: call.data == "load_xml")
 def load_xml_proceed(call):
-    user_id = call.message.chat.id
     message_text = "Пришли XML файл с данными функционального блока:"
-    bot.send_message(user_id, message_text)
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton('Отмена', callback_data='back_to_main'))
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_text, reply_markup=kb)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "help")
-def load_xml_proceed(call):
-    user_id = call.message.chat.id
+def help_proceed(call):
     message_text = '''Инструкция по использованию бота:
 1. В главном меню бота нажать на кнопку "Загрузить XML"
 2. После ответа бота загрузить XML файл функционального блока IEC-61499
 3. Дождаться генерации файла. Бот представит на выбоор 2 варианта: PNG и SVG
 
 По любым вопросам: @aToTheStars'''
-    bot.send_message(user_id, message_text)
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton('Назад', callback_data='back_to_main'))
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_text, reply_markup=kb)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "project_about")
-def load_xml_proceed(call):
-    user_id = call.message.chat.id
+def project_about_proceed(call):
     message_text = '''Бот выполнен в рамках курсовой работы для генерации подробного изображения принципиальной схемы функционального блока IEC-61499 на основе его XML файла.
 Разработчики:
+
 @aToTheStars
 @MangoWkusnoe'''
-    bot.send_message(user_id, message_text)
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton('Назад', callback_data='back_to_main'))
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_text, reply_markup=kb)
 
 
 bot.infinity_polling()
